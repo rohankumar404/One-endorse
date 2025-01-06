09b32d723cadc46e585e7981a6f78efa0ed999e9
rohankumar404 <rl.cse.rohan@gmail.com>
2025-01-06 11:51:06 +0530

    pages upload

---
+++ one-endorse/resources/views/pages/about.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.app')
+<link rel="stylesheet" href="{{ asset('assets/default/css/about.css') }}">
+<section class="hero-section py-80">
+    <div class="container">
+        <div class="row pt-4">
+            <div class="col-12 d-flex flex-column align-items-center">
+                <h1 class="font-55 text-800 text-center text-color-primary">Elevating Excellence</h1>
+                <h2 class="font-40 text-300 text-center mt-2">Empowering Connections, Inspiring Success</h2>
+                <p class="text-center font-16 text-normal w-lg-60 mt-3">Guided by a commitment to innovation and collaboration, we pave the way for impactful partnerships in the realms of sports and entertainment. Join us on the journey to redefine excellence and inspire a world of boundless possibilities</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mt-4">
+                <div class="top-image">
+                    <img class="rounded-ex-adv" src="../assets/img/images/about-img.png" alt="About image">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- our-story section  -->
+<section class="our-story bg-black py-80">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6">
+                <div class="section-head mb-3">
+                    <span class="section-tag text-white">Our Story</span>
+                    <h3 class="font-34 text-800 text-white mt-3">Creating Legacies</h3>
+                    <p class="font-30 text-300 text-white mt-3">Together, We Write Stories of <br> Triumph and Transformation</p>
+                </div>
+                <div class="p-4">
+                    <img src="../assets/img/icon/elements/Layer_1.png" alt="elements">
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 d-flex flex-column justify-content-between">
+                <div class="d-flex">
+                    <div class="col-2 elemants">
+                        <img src="../assets/img/icon/elements/Relume.png" alt="elements">
+                    </div>
+                    <div class=" col-10 our-story-content d-flex justify-content-between">
+                        <div class="d-cont text-white pl-20">
+                            <span class="font-12 text-white text-normal">2007 • December</span>
+                            <h4 class="font-24 text-600">HEstablish the one endorse</h4>
+                            <p class="font-14 text-normal">The company was founded with a vision to revolutionize the sports marketing landscape, setting the stage for a journey fueled by passion, innovation, and a commitment to excellence.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mt-4">
+                    <div class="col-2 elemants-t">
+                        <img src="../assets/img/icon/elements/Relume.png" alt="elements">
+                    </div>
+                    <div class=" col-10 our-story-content d-flex justify-content-between">
+                        <div class="d-cont text-white pl-20">
+                            <span class="font-12 text-white text-normal">2014 • June</span>
+                            <h4 class="font-24 text-600">Early Milestones</h4>
+                            <p class="font-14 text-normal">Achieving early milestones, the company gained traction in the industry by securing key partnerships and establishing a solid foundation.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mt-4">
+                    <div class="col-2 elemants-t">
+                        <img src="../assets/img/icon/elements/Relume.png" alt="elements">
+                    </div>
+                    <div class=" col-10 our-story-content d-flex justify-content-between">
+                        <div class="d-cont text-white pl-20">
+                            <span class="font-12 text-white text-normal">2020 • July</span>
+                            <h4 class="font-24 text-600">Strategic Expansion</h4>
+                            <p class="font-14 text-normal">Building on initial successes, the company entered a phase of strategic expansion, broadening its reach and Sports Celebs in the sports and entertainment sectors.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mt-4">
+                    <div class="col-2 elemants-t">
+                        <img src="../assets/img/icon/elements/Relume.png" alt="elements">
+                    </div>
+                    <div class=" col-10 our-story-content d-flex justify-content-between">
+                        <div class="d-cont text-white pl-20">
+                            <span class="font-12 text-white text-normal">2021 • November</span>
+                            <h4 class="font-24 text-600">Innovation and Impact</h4>
+                            <p class="font-14 text-normal">Embracing a culture of innovation, the company introduced cutting-edge solutions and initiatives that left a lasting impact on the industry.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mt-4">
+                    <div class="col-2 elemants-t">
+                        <img src="../assets/img/icon/elements/Relume.png" alt="elements">
+                    </div>
+                    <div class=" col-10 our-story-content d-flex justify-content-between">
+                        <div class="d-cont text-white pl-20">
+                            <span class="font-12 text-white text-normal">2023 • January</span>
+                            <h4 class="font-24 text-600">Global Recognition</h4>
+                            <p class="font-14 text-normal">Attaining global recognition, the company's journey culminated in widespread acclaim for its transformative contributions to sports marketing.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- our vision section -->
+ <section class="our-vision py-80">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-column align-items-center">
+                <span class="section-tag">Our Vision</span>
+                <p class="font-16 text-black text-700 text-center mt-3 w-lg-50">“To be the catalyst of transformative experiences in the sports and entertainment landscape, pioneering innovative collaborations that inspire, connect, and leave an indelible mark on the world.”</p>
+            </div>
+        </div>
+    </div>
+ </section>
+ <!-- our team -->
+<section class="out-team py-80">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <span class="section-tag">Team</span>
+                <h3 class="font-34 text-800 text-black mt-1">Out team</h3>
+                <p class="font-20 text-normal">Lorem ipsum dolor sit amet, ConnectEDU advising elite. </p>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center mt-5">
+            <div class="col-12 col-sm-6 col-md-6 col-lg-4 mt-20">
+                <div class="team-card shadow-buttom pb-4 rounded-mid">
+                    <div class="team-img">
+                        <img class="rounded-adv" src="https://img.freepik.com/premium-photo/man-wearing-denim-jacket-that-says-he-is-smiling_1143317-6151.jpg?w=740" alt="Team Image">
+                        <span class="badge p-0">
+                            <img src="../assets/img/icon/elements/badge.png" alt="badge">
+                        </span>
+                    </div>
+                    <div class="t-card-cnt pt-3">
+                        <h4 class="team-name font-24 text-700">Sarthak Shah</h4>
+                        <span class="font-12 text-normal">Co-Founder & CEO</span>
+                        <p class="font-14 text-normal mt-3">Driving brand visibility and engagement, a Marketing Representative orchestrates compelling campaigns and initiatives, fostering connections with target audiences to elevate brand presence.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-6 col-lg-4 mt-20">
+                <div class="team-card shadow-buttom pb-4 rounded-mid">
+                    <div class="team-img">
+                        <img class="rounded-adv" src="https://img.freepik.com/premium-photo/man-wearing-denim-jacket-that-says-he-is-smiling_1143317-6151.jpg?w=740" alt="Team Image">
+                        <span class="badge p-0">
+                            <img src="../assets/img/icon/elements/badge.png" alt="badge">
+                        </span>
+                    </div>
+                    <div class="t-card-cnt pt-3">
+                        <h4 class="team-name font-24 text-700">Sarthak Shah</h4>
+                        <span class="font-12 text-normal">Co-Founder & CEO</span>
+                        <p class="font-14 text-normal mt-3">Driving brand visibility and engagement, a Marketing Representative orchestrates compelling campaigns and initiatives, fostering connections with target audiences to elevate brand presence.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-6 col-lg-4 mt-20">
+                <div class="team-card shadow-buttom pb-4 rounded-mid">
+                    <div class="team-img">
+                        <img class="rounded-adv" src="https://img.freepik.com/premium-photo/man-wearing-denim-jacket-that-says-he-is-smiling_1143317-6151.jpg?w=740" alt="Team Image">
+                        <span class="badge p-0">
+                            <img src="../assets/img/icon/elements/badge.png" alt="badge">
+                        </span>
+                    </div>
+                    <div class="t-card-cnt pt-3">
+                        <h4 class="team-name font-24 text-700">Sarthak Shah</h4>
+                        <span class="font-12 text-normal">Co-Founder & CEO</span>
+                        <p class="font-14 text-normal mt-3">Driving brand visibility and engagement, a Marketing Representative orchestrates compelling campaigns and initiatives, fostering connections with target audiences to elevate brand presence.</p>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
+</section>
---
+++ one-endorse/resources/views/pages/faqs.blade.php
@@ -0,0 +1,2 @@
+@extends('layouts.app')
+<h1>Faqs</h1>
